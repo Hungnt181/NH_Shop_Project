c58aace5bde7a1e1df389e5a29fff937307cdfbb
Hungnt181 <ngothehung02@gmail.com>
2024-09-28 11:04:46 +0700

    completed conntact about

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import ShopPage from "./pages/(website)/shop/page";
 import ProductDetailPage from "./pages/(website)/productDetail/page";
 import CartPage from "./pages/(website)/cart/page";
 import PaymentPage from "./pages/(website)/payment/page";
+import ContactPage from "./pages/(website)/contact/page";
+import AboutPage from "./pages/(website)/about/page";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
           <Route path="/product_detail" element={<ProductDetailPage />} />
           <Route path="/wishlist" element={<CartPage />} />
           <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/about" element={<AboutPage />} />
         </Route>
 
         <Route path="admin" element={<LayoutAdmin />}>
